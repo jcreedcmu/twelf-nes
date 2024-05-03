4e83a8533b1106876b44e451a32565f0a6ccc5cf
Jason Reed <jcreed@gmail.com>
2024-05-03 13:55:01 -0400

    Big cleanup

--- iie/display_string.s
+++ iie/display_string.s
@@ -2,11 +2,7 @@
 	.case			on
 	.import		_cputs
 	.import		_cputhex8
-	.export		_display_string
 	.export		_init_serial
-	.export		_print_serial
-	.export		_print_serial_ack
-	.export		_read_cmd
 	.export		_read_cmd_loop
 
 slot					= $20
@@ -18,55 +14,27 @@ serial_status		= $C089+slot
 scratch = $60
 cmd_addr1 = scratch+0
 cmd_addr2 = scratch+1
-cmd_val = scratch+2
 loop_continue = scratch+3
 
-.segment	"RODATA"
-
-msg:
-	.byte	"Hello from asm!"
-crnl:
-   .byte 13, 10, 0
-msg2:
-	.byte	"Fetch serial ack", 13, 10, 0
-msg3:
-	.byte	"Three acks!", 13, 10, 0
+; .segment	"RODATA"
 
 .segment	"CODE"
 
-.proc	_display_string: near
-	lda     #<(msg)
-	ldx     #>(msg)
-	jsr     _cputs
-	rts
-.endproc
-
-
 	;; returns serial byte in A
-fetch_serial:
+.proc fetch_serial_ack: near
+loop:
 	lda #$08 						  ; receive register full?
 	bit serial_status
-	beq fetch_serial
-	lda serial_data
-	rts
-
-fetch_serial_ack:
-	lda #<(msg2)
-	ldx #>(msg2)
-	jsr _cputs
-fetch_serial_ack_loop:
-	lda #$08 						  ; receive register full?
-	bit serial_status
-	beq fetch_serial_ack_loop
+	beq loop
 	lda serial_data
 	tax
 	lda #$ac							  ; "ack"
 	sta serial_data
 	txa
 	rts
+.endproc
 
 .proc	_init_serial: near
-
    ;; DTR/DSR state change still producing interrupts when dip switch
 	;; is on and ProDOS doesn't like this if we haven't installed a
 	;; signal handler. We're going to be doing all serial communication
@@ -77,41 +45,6 @@ fetch_serial_ack_loop:
 	sta serial_ctl1
 	lda #$9e
 	sta serial_ctl2
-   rts
-
-.endproc
-
-.proc _print_serial: near
-	jsr fetch_serial
-	jsr _cputhex8
-	lda #<(crnl)
-	ldx #>(crnl)
-	jsr _cputs
-   rts
-.endproc
-
-.proc _print_serial_ack: near
-	jsr fetch_serial_ack
-	jsr _cputhex8
-	lda #<(crnl)
-	ldx #>(crnl)
-	jsr _cputs
-   rts
-.endproc
-
-.proc _read_cmd: near
-	jsr fetch_serial_ack
-	sta cmd_addr1
-	jsr fetch_serial_ack
-	sta cmd_addr2
-	jsr fetch_serial_ack
-   sta cmd_val
-	lda #<(msg3)
-	ldx #>(msg3)
-	jsr _cputs
-	ldy #0
-   lda cmd_val
-	sta (cmd_addr1),y
    rts
 .endproc
 
@@ -128,5 +61,6 @@ loop:
 	sta (cmd_addr1),y
 	lda loop_continue
    bne loop
+	jmp $6000
    rts
 .endproc
--- iie/hello.c
+++ iie/hello.c
@@ -1,72 +1,12 @@
 #include <apple2enh.h>
 #include <conio.h>
 
-#define PEEK(addr) (*((unsigned char *)addr))
-#define POKE(addr, val) ((*((unsigned char *)addr)) = val)
-
-void enter_hires_mode() {
-  unsigned char x;
-
-  // https://www.xtof.info/hires-graphics-apple-ii.html
-
-  x = PEEK(0xC050); // in order to switch to the graphical display
-  x = PEEK(0xC052); // to be full-screen
-  x = PEEK(0xC057); // to enter HIRES mode
-  x = PEEK(0xC054); // to select PAGE 1 or 0xC055 to select PAGE 2
-
-  // write some data to hires memory
-  POKE(0x2081, 0xff);
-}
-
 // implemented in asm
 void init_serial(void);
-void display_string(void);
-void print_serial(void);
-void print_serial_ack(void);
-void read_cmd(void);
 void read_cmd_loop(void);
 
 void main(void) {
-  unsigned char i;
-
-  display_string();
-
-  while (1) {
-	 cputs("> ");
-	 i = cgetc();
-	 if (i == 'I') {
-		init_serial();
-		cputs("Ok, initialized serial.\r\n");
-	 }
-	 else if (i == 'R') {
-		cputs("read/write register: ");
-		i = PEEK(0xC0A8);
-		cputhex8(i);
-		cputs("\r\n");
-	 }
-	 else if (i == 'S') {
-		cputs("status byte: ");
-		cputhex8(PEEK(0xC0A9));
-		cputs("\r\n");
-	 }
-	 else if (i == 'P') {
-		print_serial();
-	 }
-	 else if (i == 'Q') {
-		print_serial_ack();
-	 }
-	 else if (i == 'C') {
-		read_cmd();
-	 }
-	 else if (i == 'L') {
-		read_cmd_loop();
-	 }
-	 else if (i == 'D') {
-		cputs("debugging byte: ");
-		cputhex8(PEEK(0x0070));
-		cputs("\r\n");
-	 }
-	 else
-		cputc(i);
-  }
+  init_serial();
+  cputs("Initialized serial.\r\n");
+  read_cmd_loop();
 }
