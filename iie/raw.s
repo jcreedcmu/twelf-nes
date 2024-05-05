ec0f03ef943c2cfc63012772ccad9f0569eabc61
Jason Reed <jcreed@gmail.com>
2024-05-05 11:50:11 -0400

    Can print out byte in text mode

--- iie/raw.s
+++ iie/raw.s
@@ -1,17 +1,11 @@
 .segment "CODE"
 
-start:
-   lda $c050
- 	lda $c052
- 	lda $c057
- 	lda $c054
-	lda #$55
-	sta $2000
-	lda #$2a
-	sta $2001
-	lda #$55
-	sta $2002
+prhex = $fdda
 
+start:
+   lda $c051
+	lda #$ab
+   jsr prhex
    rts
 
 ;; start:
