d8e91fb64e831565a03422b457dd9b8fb73d9d86
Jason Reed <jcreed@gmail.com>
2024-05-05 12:13:00 -0400

    Can call monitor routines reliably

--- iie/raw.s
+++ iie/raw.s
@@ -1,12 +1,29 @@
 .segment "CODE"
 
-prhex = $fdda
+prhex = $fdda ;; print accumulator as hex
+crout = $fd8e ;; print carriage return
+text_mode = $c051
+
+;; According to http://www.applelogic.org/files/AIIETECHREF2.pdf
+;; page 82 (pdf page 12) Table 4-6
+;; we can read from this to ensure that we're reading from ROM
+;; in the range that contains system monitor routines.
+rom_switch = $c08a
 
 start:
-   lda $c051
-	lda #$ab
+	lda rom_switch
+   lda text_mode
+   lda #$cd
    jsr prhex
-   rts
+   jsr crout
+	rts
+
+
+   ;; lda $c051
+	;; lda #$ab
+   ;; jsr prhex
+   ;; jsr crout
+   ;; rts
 
 ;; start:
 ;;    lda   $c050
