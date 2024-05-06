1b6d66a161a91c38923d07df7b26b4ccff2b59d0
Jason Reed <jcreed@gmail.com>
2024-05-06 10:47:43 -0400

    Delete attempt at applepi-nix

--- applepi-nix/README.md
+++
@@ -1,8 +0,0 @@
-applepi-nix
------------
-
-An abortive attempt at trying to package applepi
-https://github.com/FZBunny/applepi
-as a nix package, but already
-https://nixos.wiki/wiki/Qt
-is completely confusing me.
--- applepi-nix/shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs = [
-                    pkgs.qt5.full
-                    pkgs.qt5.qtgamepad
-                    pkgs.qtcreator
-                  ];
-}
