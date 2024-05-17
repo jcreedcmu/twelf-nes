29e0d8df84cd1039792bad2ada07c6e288c50afd
Jason Reed <jcreed@gmail.com>
2024-05-17 10:49:14 -0400

    Add some notes

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
   with pkgs; with builtins; let
     myShellHook = ''
+    TEXINPUTS=.:/home/jcreed/dot-emacs/tex//:
     '';
   in
     mkShell {
       nativeBuildInputs = [
         cc65
+        texlive.combined.scheme-medium
       ];
       shellHook = myShellHook;
 }
